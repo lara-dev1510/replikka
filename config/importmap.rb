c270d53f04daab1649013f1dc4f260813e6ba301
Joana Lopes <joana.lps@icloud.com>
2023-11-29 13:06:33 +0100

    added carrousel

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,5 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
+pin "swiper/bundle", to: "https://ga.jspm.io/npm:swiper@11.0.5/swiper-bundle.mjs"
+pin "swiper", to: "https://ga.jspm.io/npm:swiper@11.0.5/swiper.mjs"

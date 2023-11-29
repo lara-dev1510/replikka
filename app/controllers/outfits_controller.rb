622255df5086c6ce9d7b52f61529a26da80542a4
Lara Elkasm <lara.elkasm@gmail.com>
2023-11-29 17:45:09 +0100

    navbar

--- app/controllers/outfits_controller.rb
+++ app/controllers/outfits_controller.rb
@@ -1,6 +1,11 @@
 class OutfitsController < ApplicationController
+
+  def new
+  end
+
   def show
-    @outfit = Outfit.find(params[:id])
+    # @outfit = Outfit.find(params[:id])
     # @pieces = @outfit.pieces
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,7 @@ Rails.application.routes.draw do
 
   get "/pieces", to: "pages#pieces", as: :pieces
 
+  get "/outfits/new", to: "outfits#new"
   get "/outfits/:id", to: "outfits#show", as: :outfit
+
 end

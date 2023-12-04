9249559916b945108b54cfdf110ef21202b44466
Mathieu Cochet <mathieu@cochet.me>
2023-12-04 16:13:16 +0100

    :recycle: cleaned weather + fallback strategy

--- app/services/weather_client.rb
+++ app/services/weather_client.rb
@@ -2,14 +2,16 @@ class WeatherClient
   include HTTParty
 
   def initialize(user)
-    @longitude = user.longitude
-    @latitude = user.latitude
-    @api_key =ENV["WEATHER_API_KEY"]
+    @longitude = user.longitude || '2.349'
+    @latitude = user.latitude || '48.853'
+    @api_key = ENV.fetch('WEATHER_API_KEY', nil)
   end
 
-  def get_weather
-    url = "https://api.openweathermap.org/data/2.5/weather?lat=#{@latitude}&lon=#{@longitude}&appid=#{@api_key}&units=metric"
+  def call
     self.class.get(url)
   end
 
+  def url
+    @url ||= "https://api.openweathermap.org/data/2.5/weather?lat=#{@latitude}&lon=#{@longitude}&appid=#{@api_key}&units=metric"
+  end
 end

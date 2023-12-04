9d1cd842d0d99fa5c50bf434ae6bbfe413fc8686
Noemie Dalex <noemie.dalex@gmail.com>
2023-12-04 12:16:20 +0100

    added code for outfit creation from pieces and look id.s

--- app/controllers/outfits_controller.rb
+++ app/controllers/outfits_controller.rb
@@ -5,23 +5,25 @@ class OutfitsController < ApplicationController
   # end
 
   def create
-    raise
-    @look = Look.find(params[:id])
+    # raise
+    @look = Look.find(params[:look_id])
     @outfit = Outfit.new(confirmed: true, look_id: @look.id)
     # rattacher les pieces au Outfit
     # attacher user id in schema
-    @outfit.user = current_user
+    # @outfit.user = current_user
 
- # @selected_pieces vient du form invisible
+ # @selected_pieces vient du form invisible dans looks/:id/explore
+    @selected_pieces = params[:pieces]
     @selected_pieces.each do |piece|
+      @piece = Piece.find(piece)
       outfit_piece = OutfitPiece.new
-      outfit_piece.piece = piece
+      outfit_piece.piece = @piece
       outfit_piece.outfit = @outfit
       outfit_piece.save
     end
 
     if @outfit.save
-      redirect_to explore_path(@look)
+      redirect_to explore_look_path(@look)
     else
       render :new, status: :unprocessable_entity
     end

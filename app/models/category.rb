f4ad3aa747c25bd5b4937c242aa6b8cef8155c26
Lara Elkasm <lara.elkasm@gmail.com>
2023-12-04 22:13:35 +0100

    fixed seed

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,52 +1,89 @@
 class Category < ApplicationRecord
   has_many :subcategories
 
+
   CATEGORIES = [
     {
       name: "Tops & t-shirts",
-      position: 1,
+      position: 1
+    },
+    {
+      name: "Sweaters",
+      position: 2
+    },
+    { name: "Blazers",
+      position: 11
+    },
+    { name: "Outerwear",
+      position: 3
+    },
+    { name: "Trousers",
+      position: 4
+    },
+    { name: "Jeans",
+      position: 5
+    },
+    { name: "Skirts",
+      position: 6
+    },
+    { name: "Shorts",
+      position: 7
+    },
+    { name: "Dresses",
+      position: 8
+    },
+    { name: "Jumpsuits & playsuits",
+      position: 9
+    },
+    { name: "Suits",
+      position: 10
+    }
+  ]
+
+  PHOTOS = [
+    {
+      name: "Tops & t-shirts",
       photo_url: 'https://res.cloudinary.com/dcequpnjy/image/upload/v1701714038/tops-PhotoRoom_1_jwjehq.png'
     },
     {
       name: "Sweaters",
-      position: 2,
       photo_url: 'https://res.cloudinary.com/dcequpnjy/image/upload/v1701708948/sweaters-PhotoRoom_ho4bzr.png'
     },
-    { name: "Blazers",
-      position: 11,
+    {
+      name: "Blazers",
       photo_url: 'https://res.cloudinary.com/dcequpnjy/image/upload/v1701708945/blazers-PhotoRoom_oxh1iq.png'
     },
-    { name: "Outerwear",
-      position: 3,
+    {
+      name: "Outerwear",
       photo_url: 'https://res.cloudinary.com/dcequpnjy/image/upload/v1701708936/outerwear-PhotoRoom_f7ef6w.png'
     },
-    { name: "Trousers",
-      position: 4,
+    {
+      name: "Trousers",
       photo_url: 'https://res.cloudinary.com/dcequpnjy/image/upload/v1701708937/trousers-PhotoRoom_cjkrfq.png'
     },
-    { name: "Jeans",
-      position: 5,
+    {
+      name: "Jeans",
       photo_url: 'https://res.cloudinary.com/dcequpnjy/image/upload/v1701708938/jeans-PhotoRoom_zv0itu.png'
     },
-    { name: "Skirts",
-      position: 6,
+    {
+      name: "Skirts",
       photo_url: 'https://res.cloudinary.com/dcequpnjy/image/upload/v1701708949/skirt-PhotoRoom_bphemc.png'
     },
-    { name: "Shorts",
-      position: 7,
+    {
+      name: "Shorts",
       photo_url: 'https://res.cloudinary.com/dcequpnjy/image/upload/v1701708940/shorts-PhotoRoom_lozxws.png'
     },
-    { name: "Dresses",
-      position: 8,
+    {
+      name: "Dresses",
       photo_url: 'https://res.cloudinary.com/dcequpnjy/image/upload/v1701708941/dress-PhotoRoom_hhhamd.png'
     },
-    { name: "Jumpsuits & playsuits",
-      position: 9,
+    {
+      name: "Jumpsuits & playsuits",
       photo_url: 'https://res.cloudinary.com/dcequpnjy/image/upload/v1701708942/jumpsuits-PhotoRoom_oo3ibe.png'
     },
-    { name: "Suits",
-      position: 10,
-      photo_url:'https://res.cloudinary.com/dcequpnjy/image/upload/v1701708946/suits_y56oqj.png'
+    {
+      name: "Suits",
+      photo_url: 'https://res.cloudinary.com/dcequpnjy/image/upload/v1701708946/suits_y56oqj.png'
     }
   ]
 
@@ -65,6 +102,6 @@ class Category < ApplicationRecord
   }
 
   def photo_url
-    CATEGORIES.find { |category| category[:name] == name }&.dig(:photo_url)
+    PHOTOS.find { |category| category[:name] == name }&.dig(:photo_url)
   end
 end

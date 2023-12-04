d50168f0ab9138458e662e165827d8f93a5796c1
Noemie Dalex <noemie.dalex@gmail.com>
2023-12-04 14:53:42 +0100

    fixed dependent destroy for pieces & outfits outfit_pieces

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -2,6 +2,7 @@ class Piece < ApplicationRecord
   belongs_to :user
   belongs_to :subcategory
   has_one_attached :photo
+  has_many :outfit_pieces, dependent: :destroy
 
   include PgSearch::Model
   pg_search_scope :global_search, against: [:name], associated_against: { subcategory: [:name, :color, :fabric] }, using: { tsearch: { prefix: true } }

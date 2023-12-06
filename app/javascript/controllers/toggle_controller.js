9b9626290c78f9ff171d785cc19b8ec95ed362a8
Mathieu Cochet <mathieu@cochet.me>
2023-12-06 23:17:01 +0100

    outfit zoom

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,32 +1,28 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["thumbnail", "matchingPieces", "title", "slider", "matchingImage"]
+  static targets = [
+    "thumbnail",
+    "slider",
+    "matchingImage"
+  ]
 
   connect() {
   }
 
   switchDisplay() {
-    this.thumbnailTarget.classList.toggle('hidden');
-    this.matchingPiecesTarget.classList.toggle('hidden');
-    if (this.thumbnailTarget.classList.contains("hidden")) {
-      this.titleTarget.innerHTML = 'Look'
-    } else {
-      this.titleTarget.innerHTML = 'Outfit'
-    }
+    this.thumbnailTarget.classList.toggle('thumbnail');
+    this.thumbnailTarget.classList.toggle('full_page_image');
   }
 
   slide(event) {
-    console.log('coucou');
-      if (event.currentTarget.value == 100) {
-        // window.location.href = '/today'
-        this.sliderTarget.remove()
-        this.thumbnailTarget.children[0].classList.add('confirmed')
-        this.matchingImageTargets.forEach( (target) => {
-          target.children[0].classList.add('confirmed')
-        })
+    if (event.currentTarget.value == 100) {
+      this.sliderTarget.remove()
+      this.thumbnailTarget.children[0].classList.add('confirmed')
+      this.matchingImageTargets.forEach( (target) => {
+        target.children[0].classList.add('confirmed')
+      })
 
-      }
-
+    }
   }
 }

6b427cd3d87339ffbb2866d1d85cd475c33cc010
Lara Elkasm <lara.elkasm@gmail.com>
2023-12-06 18:00:38 +0100

    ok

---
+++ app/javascript/controllers/exploreswiper_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "@hotwired/stimulus"
+import Swiper from 'swiper/bundle';
+
+export default class extends Controller {
+  static values = {
+    slidesPerView: { type: Number, default: 3 },
+    loop: { type: Boolean, default: false }
+  }
+
+  connect() {
+    const swiper = new Swiper(this.element, {
+      slidesPerView: this.slidesPerViewValue,
+      spaceBetween: 1,
+      freeMode: true,
+      loop: this.loopValue,
+    });
+  }
+}

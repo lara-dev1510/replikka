0c003a9055f154549d69315c7303de2308845af4
Mathieu Cochet <mathieu@cochet.me>
2023-12-06 23:38:48 +0100

    Cleaned console.log + fixed contained height

--- app/javascript/controllers/searchbar_controller.js
+++ app/javascript/controllers/searchbar_controller.js
@@ -6,18 +6,5 @@ export default class extends Controller {
 
   connect() {}
 
-  search(event) {
-    
-    console.log(event.key)
-    // this.event.innerText = " "
-    // this.currentTarget.innerText = this.event
-    // this.currentTarget.innerText = this.input
-    // this.event = this.key.currentTarget
-  }
-  // const drawResponseList = (data) => {
-  //   results.innerHTML = '';
-  //   data.words.forEach((word) => {
-  //     results.insertAdjacentHTML('beforeend', `<li>${word}</li>`);
-  //   });
-  // };
+  search(event) {}
 }
